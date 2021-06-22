5dd62507f964f0d506c8cfdd0728fe99eab53605
agustin-montiveros <agus.monti14@outlook.com>
2021-06-21 23:53:20 -0300

    Agregado lo de renombrar el archivo

--- tp6ej4.py
+++ tp6ej4.py
@@ -3,7 +3,7 @@
 # Agustin Montiveros - @agustin-montiveros
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
-
+import shutil, os
 
 
 def codificador(archivo, cifrado):
@@ -19,7 +19,17 @@ def principal():
         with open(archivo, 'r', encoding="utf8") as archivo_original:
             arch = archivo_original.read()
             mostrar = codificador(arch, cifrado)
-            print(mostrar)
+            archivo_original.close()
+        with open(archivo, 'w', encoding="utf8") as archivo_original:
+            archivo_original.write(mostrar)
+            archivo_original.close()
+            ruta = os.getcwd() + os.sep
+            origen = ruta + archivo
+            destino = ruta + 'cifrado_prueba.cesar.txt'
+            archivo_a = origen
+            nombre_nuevo = destino
+            os.rename(archivo_a, nombre_nuevo)
+            
     except FileNotFoundError as error:
         print("El archivo no se encontró. (También se debe poner el tipo de archivo (ej: archivo.txt))\n", error)
 
