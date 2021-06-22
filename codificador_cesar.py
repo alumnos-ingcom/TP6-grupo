e58de4feb8bdbeb72eb652927ffac3201e0eb733
B4zy <togadea@gmail.com>
2021-06-21 21:37:31 -0300

    Codificador Cesar
    Es la funcion para codificar en cesar el punto 4

---
+++ codificador_cesar.py
@@ -0,0 +1,17 @@
+################
+# Tomás Gadea - @B4zy
+# Agustin Montiveros - @agustin-montiveros
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+
+
+def codificar_cesar(texto, cifrado):
+    texto = list(texto)
+    texto_cesar = []
+    for i in range(len(texto)):
+        numero = ord(texto[i])
+        numero_cifrado = numero + cifrado
+        letra_cifrada = chr(numero_cifrado)
+        texto_cesar.append(letra_cifrada)
+    return "".join(texto_cesar)
