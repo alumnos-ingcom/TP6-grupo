d75112c5ce53efe7cb3d66fb88fa7eb0a49465ec
agustin-montiveros <agus.monti14@outlook.com>
2021-06-22 00:45:13 -0300

    con comentarios

--- tp6ej3.py
+++ tp6ej3.py
@@ -9,15 +9,15 @@ import shutil, os
 
         
 def principal():
-    ruta = os.getcwd() + os.sep
-    origen = ruta + 'plantilla.py'
-    destino = ruta + 'tp6ejX.py'#------>CAMBIAR LA X POR EL N° DEL EJERCICIO DESEADO
+    ruta = os.getcwd() + os.sep #busca la ruta de los archivos con esos metodos
+    origen = ruta + 'plantilla.py'  #da la ruta del archivo origen mas el nombre del archivo origen
+    destino = ruta + 'tp6ejX.py'#------>CAMBIAR LA X POR EL N° DEL EJERCICIO DESEADO, da la ruta del archivo destino + el nombre del archivo destino
 
-    if os.path.exists(origen):
-        with open(origen, 'rb') as archivo_origen:
-            with open(destino, 'wb') as archivo_destino:
-                shutil.copyfileobj(archivo_origen, archivo_destino)
-                print("Archivo copiado")
+    if os.path.exists(origen): #da true para que el if funcione, es si existe
+        with open(origen, 'rb') as archivo_origen: #abre el archivo como archivo_origen
+            with open(destino, 'wb') as archivo_destino: #abre el archivo destino
+                shutil.copyfileobj(archivo_origen, archivo_destino) #copia el contenido de un archivo origen al archivo destino
+                print("Archivo copiado") #avisa que se copio el archivo
 
 if __name__ == "__main__":
     principal()
